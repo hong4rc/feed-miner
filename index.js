6c95d5ef58fb07c9dc6d7a2077e6efcd0f9d600a
Hongarc <anhhong12121997t@gmail.com>
2019-03-06 12:18:39 +0700

    :package: lint core

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-'use strict';
+
 require('dotenv').config();
 
 const graph = require('./lib/graph');
@@ -6,21 +6,21 @@ const log = require('./lib/log');
 
 let process = graph.getListFriend();
 const now = new Date();
-process.then(users => {
-    for (const user of users) {
-        log.info(user);
-        process = process.then(() => graph.loadFeed(user.id, user.name))
-            .then(feeds => {
-                for (const feed of feeds) {
-                    const that = new Date(feed.created_time);
-                    if (that.getDate() === now.getDate()
-                        && that.getMonth() === now.getMonth()
-                        && that.getFullYear() !== now.getFullYear()
-                        && feed.id.match(new RegExp(`^${user.id}`))
-                    ) {
-                        log.info(user.name, `https://www.facebook.com/${feed.id}`);
-                    }
-                }
-            });
-    }
+process.then((users) => {
+  users.forEach((user) => {
+    log.info(user);
+    process = process.then(() => graph.loadFeed(user.id, user.name))
+      .then((feeds) => {
+        feeds.forEach((feed) => {
+          const that = new Date(feed.created_time);
+          if (that.getDate() === now.getDate()
+                              && that.getMonth() === now.getMonth()
+                              && that.getFullYear() !== now.getFullYear()
+                              && feed.id.match(new RegExp(`^${user.id}`))
+          ) {
+            log.info(user.name, `https://www.facebook.com/${feed.id}`);
+          }
+        });
+      });
+  });
 });
