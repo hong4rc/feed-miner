54c9f3035ba0367ddb2bf8736f93f44b20173384
Hongarc <anhhong12121997t@gmail.com>
2019-03-06 12:29:21 +0700

    :loud_sound: change log

--- index.js
+++ index.js
@@ -1,15 +1,14 @@
 
 require('dotenv').config();
-
+const log = require('debug')('bot');
 const graph = require('./lib/graph');
-const log = require('./lib/log');
 
 let process = graph.getListFriend();
 const now = new Date();
 process.then((users) => {
   users.forEach((user) => {
-    log.info(user);
-    process = process.then(() => graph.loadFeed(user.id, user.name))
+    log(user);
+    process = process.then(() => graph.loadFeed(user.id))
       .then((feeds) => {
         feeds.forEach((feed) => {
           const that = new Date(feed.created_time);
@@ -18,7 +17,7 @@ process.then((users) => {
                               && that.getFullYear() !== now.getFullYear()
                               && feed.id.match(new RegExp(`^${user.id}`))
           ) {
-            log.info(user.name, `https://www.facebook.com/${feed.id}`);
+            log(user.name, `https://www.facebook.com/${feed.id}`);
           }
         });
       });
